abfbb91e3679663d44f13d690828a36d23bfdf85
Qianxy <xyqian67@gmail.com>
2023-04-21 11:27:05 -0700

    Format

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = { window = { blend = 0 } } },
+      { 'j-hui/fidget.nvim',       opts = { window = { blend = 0 } } },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
